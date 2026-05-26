e3852184ec3a38cbee8fe62075aa43951714311a
tanayarun <ankulwartanay@gmail.com>
2026-05-26 17:17:38 +0530

    reaper goroutine

---
+++ reaper.go
@@ -0,0 +1,35 @@
+package aquifer
+
+import "time"
+
+func (p *Pool[C]) reaper() {
+	ticker := time.NewTicker(p.cfg.idleTimeout / 2)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			p.evictExpired()
+		case <-p.done:
+			return
+		}
+	}
+}
+
+func (p *Pool[C]) evictExpired() {
+	n := int64(len(p.idle))
+
+	for i := int64(0); i < n; i++ {
+		select {
+		case conn := <-p.idle:
+			if conn.isExpired(p.cfg.idleTimeout) && p.open.Load() > int64(p.cfg.minConns) {
+				conn.Close()
+				p.open.Add(-1)
+			} else {
+				p.idle <- conn
+			}
+		default:
+			return
+		}
+	}
+}
